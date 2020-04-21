9274eafc6e1b865bbe90b352f0659adaa32edd81
pcarroll1386 <pfcarroll13@gmail.com>
2020-04-21 16:10:45 -0500

    added tests for User

--- inventorytracker/src/test/java/com/pfc/inventorytracker/dao/RoleDBTest.java
+++ inventorytracker/src/test/java/com/pfc/inventorytracker/dao/RoleDBTest.java
@@ -11,12 +11,19 @@ import com.pfc.inventorytracker.entities.Location;
 import com.pfc.inventorytracker.entities.Request;
 import com.pfc.inventorytracker.entities.Role;
 import com.pfc.inventorytracker.entities.User;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -97,20 +104,38 @@ public class RoleDBTest {
      */
     @Test
     public void testGetAllRoles() {
+        Role role = new Role();
+        role.setRole("ROLE_TEST");
+        role = roleDao.addRole(role);
+        
+        Role role2 = new Role();
+        role2.setRole("ROLE_TEST2");
+        role2 = roleDao.addRole(role2);
+        
+        Set<Role> roles = new HashSet<>();
+        roles.add(role);
+        roles.add(role2);
+        
+        User user = new User();
+        user.setUsername("test username");
+        user.setPassword("test password");
+        user.setEnabled(true);
+        user.setRoles(roles);        
+        user = userDao.addUser(user);
+        
+        List<Role> fromDao = roleDao.getAllRoles();
+        
+        assertEquals(2, fromDao.size());
+        assertTrue(fromDao.contains(role));
+        assertTrue(fromDao.contains(role2));
+        
     }
 
     /**
-     * Test of getRoleById method, of class RoleDB.
+     * Test of addRole method & getRoleById method, of class RoleDB.
      */
     @Test
-    public void testGetRoleById() {
-    }
-
-    /**
-     * Test of addRole method, of class RoleDB.
-     */
-    @Test
-    public void testAddRole() {
+    public void testAddGetRoleById() {
     }
 
     /**
--- inventorytracker/src/test/java/com/pfc/inventorytracker/dao/UserDBTest.java
+++ inventorytracker/src/test/java/com/pfc/inventorytracker/dao/UserDBTest.java
@@ -11,13 +11,21 @@ import com.pfc.inventorytracker.entities.Location;
 import com.pfc.inventorytracker.entities.Request;
 import com.pfc.inventorytracker.entities.Role;
 import com.pfc.inventorytracker.entities.User;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -97,20 +105,66 @@ public class UserDBTest {
      */
     @Test
     public void testGetAllUsers() {
+        Role role = new Role();
+        role.setRole("ROLE_TEST");
+        role = roleDao.addRole(role);
+        
+        Role role2 = new Role();
+        role2.setRole("ROLE_TEST2");
+        role2 = roleDao.addRole(role2);
+        
+        Set<Role> roles = new HashSet<>();
+        roles.add(role);
+        roles.add(role2);
+        
+        User user = new User();
+        user.setUsername("test username");
+        user.setPassword("test password");
+        user.setEnabled(true);
+        user.setRoles(roles);        
+        user = userDao.addUser(user);
+        
+        User user2 = new User();
+        user.setUsername("test username2");
+        user.setPassword("test password2");
+        user.setEnabled(false);
+        user.setRoles(roles);
+        
+        List<User> users = userDao.getAllUsers();
+        
+        assertEquals(2, users.size());
+        assertTrue(users.contains(user));
+        assertTrue(users.contains(user2));
     }
 
     /**
-     * Test of getUserByUsername method, of class UserDB.
+     * Test of getUserByUsername method & getUserByUsername method, of class UserDB.
      */
     @Test
-    public void testGetUserByUsername() {
-    }
-
-    /**
-     * Test of addUser method, of class UserDB.
-     */
-    @Test
-    public void testAddUser() {
+    public void testAddGetUser() {
+        Role role = new Role();
+        role.setRole("ROLE_TEST");
+        role = roleDao.addRole(role);
+        
+        Role role2 = new Role();
+        role2.setRole("ROLE_TEST2");
+        role2 = roleDao.addRole(role2);
+        
+        Set<Role> roles = new HashSet<>();
+        roles.add(role);
+        roles.add(role2);
+        
+        User user = new User();
+        user.setUsername("test username");
+        user.setPassword("test password");
+        user.setEnabled(true);
+        user.setRoles(roles);
+        
+        user = userDao.addUser(user);
+        
+        User fromDao = userDao.getUserByUsername(user.getUsername());
+        
+        assertEquals(user, fromDao);
     }
 
     /**
@@ -118,6 +172,42 @@ public class UserDBTest {
      */
     @Test
     public void testUpdateUser() {
+        Role role = new Role();
+        role.setRole("ROLE_TEST");
+        role = roleDao.addRole(role);
+        
+        Role role2 = new Role();
+        role2.setRole("ROLE_TEST2");
+        role2 = roleDao.addRole(role2);
+        
+        Role role3 = new Role();
+        role3.setRole("ROLE_TEST3");
+        role3 = roleDao.addRole(role3);
+        
+        Set<Role> roles = new HashSet<>();
+        roles.add(role);
+        roles.add(role2);
+        
+        User user = new User();
+        user.setUsername("test username");
+        user.setPassword("test password");
+        user.setEnabled(true);
+        user.setRoles(roles);
+        
+        user = userDao.addUser(user);        
+        User fromDao = userDao.getUserByUsername(user.getUsername());        
+        assertEquals(user, fromDao);
+        
+        roles.remove(role2);
+        roles.add(role3);        
+        user.setRoles(roles);
+        userDao.updateUser(user);
+        
+        assertNotEquals(user, fromDao);
+        
+        fromDao = userDao.getUserByUsername(user.getUsername());
+        
+        assertEquals(user, fromDao);
     }
 
     /**
@@ -125,6 +215,76 @@ public class UserDBTest {
      */
     @Test
     public void testDeleteUser() {
+        Role role = new Role();
+        role.setRole("ROLE_TEST");
+        role = roleDao.addRole(role);
+        
+        Role role2 = new Role();
+        role2.setRole("ROLE_TEST2");
+        role2 = roleDao.addRole(role2);
+        
+        Set<Role> roles = new HashSet<>();
+        roles.add(role);
+        roles.add(role2);
+        
+        User user = new User();
+        user.setUsername("test username");
+        user.setPassword("test password");
+        user.setEnabled(true);
+        user.setRoles(roles);        
+        user = userDao.addUser(user);
+        
+        Category category = new Category();
+        category.setName("test name");
+        category = categoryDao.addCategory(category);
+        
+        Category category2 = new Category();
+        category2.setName("test2 name");
+        category2 = categoryDao.addCategory(category2);
+        
+        Set<Category> categories = new HashSet<>();
+        categories.add(category);
+        categories.add(category2);
+        
+        Item item = new Item();
+        item.setName("test name");
+        item.setDescription("test description");
+        item.setInInventory(5);
+        item.setMax(10);
+        item.setMin(4);
+        item.setQuantity(5);
+        item.setName("test nickname");
+        item.setPrice(new BigDecimal("25.56"));
+        item.setCategories(categories);
+        item = itemDao.addItem(item);
+        
+        Item item2 = new Item();
+        item2.setName("test2 name");
+        item2.setDescription("test2 description");
+        item2.setInInventory(5);
+        item2.setMax(10);
+        item2.setMin(4);
+        item2.setQuantity(5);
+        item2.setName("test2 nickname");
+        item2.setPrice(new BigDecimal("25.97"));
+        item2.setCategories(categories);
+        item2 = itemDao.addItem(item2);
+        
+        List<Item> items = new ArrayList<>();
+        
+        
+        Location location = new Location();
+        location.setName("Test name");
+        location.setUser(user);
+        location.setDescription("test Description");
+        location.setItems(items);
+        location = locationDao.addLocation(location);
+        
+        userDao.deleteUser(user.getUsername());
+            
+        User fromDao = userDao.getUserByUsername(user.getUsername());
+        
+        assertNull(fromDao);
     }
 
     /**
@@ -132,6 +292,52 @@ public class UserDBTest {
      */
     @Test
     public void testGetAllUsersByRole() {
+        Role role = new Role();
+        role.setRole("ROLE_TEST");
+        role = roleDao.addRole(role);
+        
+        Role role2 = new Role();
+        role2.setRole("ROLE_TEST2");
+        role2 = roleDao.addRole(role2);
+        
+        Set<Role> roles = new HashSet<>();
+        roles.add(role);
+        roles.add(role2);
+        
+        User user = new User();
+        user.setUsername("test username");
+        user.setPassword("test password");
+        user.setEnabled(true);
+        user.setRoles(roles);        
+        user = userDao.addUser(user);
+        
+        User user2 = new User();
+        user.setUsername("test username2");
+        user.setPassword("test password2");
+        user.setEnabled(false);
+        user.setRoles(roles);
+        
+        roles.remove(role2);
+        
+        User user3 = new User();
+        user.setUsername("test username3");
+        user.setPassword("test password3");
+        user.setEnabled(false);
+        user.setRoles(roles);
+        
+        List<User> users = userDao.getAllUsersByRole(role);
+        
+        assertEquals(3, users.size());
+        assertTrue(users.contains(user));
+        assertTrue(users.contains(user2));
+        assertTrue(users.contains(user3));
+        
+        users = userDao.getAllUsersByRole(role2);
+        
+        assertEquals(2, users.size());
+        assertTrue(users.contains(user));
+        assertTrue(users.contains(user2));
+        assertFalse(users.contains(user3));
     }
     
 }
